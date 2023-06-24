8a3653ff74d5735b8be625aa96ca74d57be683d5
vipul440 <82093511+vipul440@users.noreply.github.com>
2023-06-24 17:05:17 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,7 @@ function Dashboard({ setLoggedIn }) {
     return (
         <div id="dashboardPage">
             <h1 id="dashboard-tag"> Welcome to Dashboard</h1>
-            <button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button>
+            <Link to="/"><button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button>
         </div>
     )
 
